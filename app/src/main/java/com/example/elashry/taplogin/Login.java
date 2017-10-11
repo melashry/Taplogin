7a1207f48f664da2ef72f4a8d407993fb7ef8788
elashry <mohamedelashry323@gmail.com>
2017-10-11 14:27:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/elashry/taplogin/Login.java
@@ -0,0 +1,29 @@
+package com.example.elashry.taplogin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by elashry on 10/11/2017.
+ */
+
+public class Login extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View rootview=inflater.inflate(R.layout.login,container,false);
+
+        return rootview;
+
+
+
+    }
+}
